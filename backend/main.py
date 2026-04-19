d8e8347f6711bec8a2ce16bb29c17bf0f1ccdab9
marcel.nagy@gmail.com <marcel.nagy@gmail.com>
2026-04-19 18:16:38 +0200

    prepare backend for render

--- backend/main.py
+++ backend/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3001",
+        "https://opravfotku.vercel.app",
     ],
     allow_credentials=False,
     allow_methods=["*"],
